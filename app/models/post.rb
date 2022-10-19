89bcdcef36687b6bc00b4b2f686b5422fe3d4fc9
Duy Pham <ericpham19@outlook.com>
2022-10-20 08:17:11 +1100

    validated

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,18 @@
 class Post < ActiveRecord::Base
+    validates :title, presence: true
+    validates :content, length: {minimum: 250}
+    validates :summary, length: {maximum: 250}
+    validates :category, inclusion: {in: ["Fiction", "Non-Fiction"]}
+    validate :validate_clickbait?
+
+    CLICKBAIT= [ /Won't Believe/i,
+    /Secret/i,
+    /Top [0-9]*/i,
+    /Guess/i]
+
+    def validate_clickbait?
+        if CLICKBAIT.none? {|patterns| patterns.match title}
+            errors.add(:title, "must be clickbait")
+        end
+    end
 end
